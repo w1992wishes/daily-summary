59c8d8f8ecf2fe75ad0750c3acb4780f9d5fb0a3
wanqinfeng <571716844@qq.com>
2018-09-21 11:40:51 +0800

    add sparkSql 连接 greenplum 实例
    Signed-off-by: wanqinfeng <571716844@qq.com>

---
+++ study-spark/spark-etl/src/main/scala/me/w1992wishes/spark/etl/GpExample.scala
@@ -0,0 +1,30 @@
+package me.w1992wishes.spark.etl
+
+import org.apache.spark.sql.SparkSession
+
+
+/**
+  * spark 连接 greenplum 例子
+  *
+  * @author w1992wishes 2018/9/18 16:38
+  */
+object GpExample {
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession
+      .builder()
+      .appName("gptest")
+      .master("local")
+      .getOrCreate()
+
+    val jdbcDF = spark.read
+      .format("jdbc")
+      .option("url", "jdbc:pivotal:greenplum://192.168.11.72:5432;DatabaseName=testdb")
+      .option("dbtable", "public.t_archive_person")
+      .option("user", "gpadmin")
+      .option("password", "gpadmin")
+      .load()
+
+    jdbcDF.where("person_id < 10").show()
+  }
+}
