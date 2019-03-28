9bfde0b93a70109bdb4935f9d113ed4f10ce14c6
wanqinfeng <wan.qinfeng@intellif.com>
2019-03-28 15:40:29 +0800

    增加重试 utils
    Signed-off-by: wanqinfeng <wan.qinfeng@intellif.com>

---
+++ utils/common-utils/src/main/java/me/w1992wishes/common/exception/MyRuntimeException.java
@@ -0,0 +1,12 @@
+package me.w1992wishes.common.exception;
+
+/**
+ * @author w1992wishes 2019/3/28 15:37
+ */
+public class MyRuntimeException extends RuntimeException {
+
+    public MyRuntimeException(String msg) {
+        super(msg);
+    }
+
+}
