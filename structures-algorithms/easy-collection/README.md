c817ba3aacfaf7d8a8ebbd7012c01dc0380f0191
w1992wishes <571716844@qq.com>
2019-06-05 11:15:08 +0800

    算法 Best Time to Buy and Sell Stock II
    Signed-off-by: w1992wishes <571716844@qq.com>

--- structures-algorithms/easy-collection/README.md
+++ structures-algorithms/easy-collection/README.md
@@ -8,4 +8,20 @@ Given a sorted array nums, remove the duplicates in-place such that each element
 
 Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
 
-### 
+给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+
+不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+
+### maxProfit
+
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
+
+Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
+
+给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+
+设计一个算法来计算你所能获取的最大利润。你可以尽可能地完成更多的交易（多次买卖一支股票）。
+
+注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
--- structures-algorithms/easy-collection/src/main/java/me/w1992wishes/easy/collection/array/Solution.java
+++ structures-algorithms/easy-collection/src/main/java/me/w1992wishes/easy/collection/array/Solution.java
@@ -1,19 +1,17 @@
 package me.w1992wishes.easy.collection.array;
 
-
 /**
- *
  * @author w1992wishes 2019/6/4 14:04
  */
 public class Solution {
 
     /**
      * Q: Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
-     *
+     * <p>
      * Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
-     *
+     * <p>
      * S: 使用快慢指针来记录遍历的坐标，最开始时两个指针都指向第一个数字，如果两个指针指的数字相同，则快指针向前走一步，
-     *
+     * <p>
      * 如果不同，则两个指针都向前走一步，这样当快指针走完整个数组后，慢指针当前的坐标加1就是数组中不同数字的个数
      */
     public int removeDuplicates(int[] nums) {
@@ -21,7 +19,9 @@ public class Solution {
             return 0;
         }
 
-        int pre = 0; int cur = 0; int n = nums.length;
+        int pre = 0;
+        int cur = 0;
+        int n = nums.length;
         while (cur < n) {
             if (nums[pre] != nums[cur]) {
                 nums[++pre] = nums[cur];
@@ -29,6 +29,58 @@ public class Solution {
             cur++;
         }
         return pre + 1;
+    }
+
+
+    /**
+     * Say you have an array for which the ith element is the price of a given stock on day i.
+     * <p>
+     * Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
+     * <p>
+     * 简单的方法就是一旦第二天的价格比前一天的高，就在前一天买入第二天卖出，但是这个会违反“不能同一天买卖的规则”
+     */
+    public int maxProfit1(int[] prices) {
+        int total = 0;
+        for (int i = 0; i < prices.length - 1; ++i) {
+            if (prices[i] < prices[i + 1]) {
+                total += prices[i + 1] - prices[i];
+            }
+        }
+        return total;
+    }
+
+    /**
+     * 低谷买，高峰卖就可以获得最大利润
+     */
+    public int maxProfit2(int[] prices) {
+        int len = prices.length;
+        if (len <= 1) {
+            return 0;
+        }
+
+        int i = 0;
+        int total = 0;
+        while (i < len - 1) {
+            int buy, sell;
+            //寻找递减区间的最后一个值（局部最小点）
+            while (i + 1 < len && prices[i + 1] < prices[i]) {
+                i++;
+            }
+            //局部最小点作为买入点
+            buy = i;
+
+            //找下一个点(卖出点至少为下一个点）
+            i++;
+            //不满足。。继续往下找递增区间的最后一个值（局部最高点）
+            while (i < len && prices[i] >= prices[i - 1]) {
+                i++;
+            }
+            //设置卖出点
+            sell = i - 1;
+            //计算总和
+            total += prices[sell] - prices[buy];
+        }
+        return total;
     }
 
 }
