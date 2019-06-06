1e5bd66fe197f918a094c241a6d3a662976c68e4
w1992wishes <571716844@qq.com>
2019-06-06 13:52:57 +0800

    二叉树中序遍历
    Signed-off-by: w1992wishes <571716844@qq.com>

--- structures-algorithms/binary-tree/src/main/java/me/w1992wishes/binary/tree/Solution.java
+++ structures-algorithms/binary-tree/src/main/java/me/w1992wishes/binary/tree/Solution.java
@@ -24,6 +24,33 @@ import java.util.*;
 class Solution {
 
     /**
+     * Given a binary tree, return the preorder traversal of its nodes' values.
+     * <p>
+     * Pre-order traversal is to visit the root first. Then traverse the left subtree. Finally, traverse the right subtree.
+     */
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> nodes = new ArrayList<>();
+        recursivePreorder(root, nodes);
+        stackPreorder(root, nodes);
+        return nodes;
+    }
+
+    /**
+     * Given a binary tree, return the inorder traversal of its nodes' values.
+     * <p>
+     * In-order traversal is to traverse the left subtree first. Then visit the root. Finally, traverse the right subtree.
+     */
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> nodes = new ArrayList<>();
+        recursiveInorder(root, nodes);
+        stackInorder(root, nodes);
+        return nodes;
+    }
+
+
+    /**
+     * 递归前序实现
+     * <p>
      * 二叉树的前序遍历是:中->左->右。采用这个次序进行递归。
      */
     private void recursivePreorder(TreeNode root, List<Integer> nodes) {
@@ -36,11 +63,13 @@ class Solution {
     }
 
     /**
+     * 栈的非递归前序实现
+     * <p>
      * 递归的思想其实就是栈思想，因此非递归版本采用栈来实现。观察递归版本可知，函数先递归求解左子树，再求解右子树。
      * <p>
      * 因而采用栈（先进后出特性）时，为了先处理左子树，再处理右子树，只能先将右子树入栈，再将左子树入栈。
      */
-    private void iterativePreorder(TreeNode root, List<Integer> nodes) {
+    private void stackPreorder(TreeNode root, List<Integer> nodes) {
         if (root == null) {
             return;
         }
@@ -59,12 +88,35 @@ class Solution {
     }
 
     /**
-     * Given a binary tree, return the preorder traversal of its nodes' values.
+     * 递归中序实现，左->中->右
      */
-    public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> nodes = new ArrayList<>();
-        recursivePreorder(root, nodes);
-        iterativePreorder(root, nodes);
-        return nodes;
+    private void recursiveInorder(TreeNode root, List<Integer> nodes) {
+        if (root == null) {
+            return;
+        }
+        recursiveInorder(root.left, nodes);
+        nodes.add(root.val);
+        recursiveInorder(root.right, nodes);
     }
+
+    /**
+     * 栈的非递归中序遍历，左->中->右
+     */
+    private void stackInorder(TreeNode root, List<Integer> nodes) {
+        if (root == null) {
+            return;
+        }
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode node = root;
+        while (node != null || !stack.isEmpty()) {
+            while (node != null) {
+                stack.push(node);
+                node = node.left;
+            }
+            node = stack.pop();
+            nodes.add(node.val);
+            node = node.right;
+        }
+    }
+
 }
