2a7c96a7739739d79a0aa5dfde534e9b57c94037
w1992wishes <571716844@qq.com>
2019-06-11 10:53:18 +0800

    Contains Duplicate
    Signed-off-by: w1992wishes <571716844@qq.com>

--- structures-algorithms/easy-collection/src/main/java/me/w1992wishes/easy/collection/array/MaxProfitArray.java
+++ structures-algorithms/easy-collection/src/main/java/me/w1992wishes/easy/collection/array/MaxProfitArray.java
@@ -1,22 +1,22 @@
 package me.w1992wishes.easy.collection.array;
 
 /**
+ * Say you have an array for which the ith element is the price of a given stock on day i.
+ * <p>
+ * Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: [7,1,5,3,6,4]
+ * Output: 7
+ * Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+ * Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+ *
  * @author w1992wishes 2019/6/4 14:04
  */
 public class MaxProfitArray {
 
     /**
-     * Say you have an array for which the ith element is the price of a given stock on day i.
-     * <p>
-     * Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
-     * <p>
-     * Example 1:
-     * <p>
-     * Input: [7,1,5,3,6,4]
-     * Output: 7
-     * Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
-     * Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
-     * <p>
      * 简单的方法就是一旦第二天的价格比前一天的高，就在前一天买入第二天卖出，但是这个会违反“不能同一天买卖的规则”
      */
     public int maxProfit1(int[] prices) {
--- structures-algorithms/easy-collection/src/main/java/me/w1992wishes/easy/collection/array/RotateArray.java
+++ structures-algorithms/easy-collection/src/main/java/me/w1992wishes/easy/collection/array/RotateArray.java
@@ -1,30 +1,25 @@
 package me.w1992wishes.easy.collection.array;
 
 /**
+ * Given an array, rotate the array to the right by k steps, where k is non-negative.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: [1,2,3,4,5,6,7] and k = 3
+ * Output: [5,6,7,1,2,3,4]
+ * Explanation:
+ * rotate 1 steps to the right: [7,1,2,3,4,5,6]
+ * rotate 2 steps to the right: [6,7,1,2,3,4,5]
+ * rotate 3 steps to the right: [5,6,7,1,2,3,4]
+ *
  * @author w1992wishes 2019/6/10 11:38
  */
 public class RotateArray {
 
     /**
-     * Given an array, rotate the array to the right by k steps, where k is non-negative.
-     * <p>
-     * Example 1:
-     * <p>
-     * Input: [1,2,3,4,5,6,7] and k = 3
-     * Output: [5,6,7,1,2,3,4]
-     * Explanation:
-     * rotate 1 steps to the right: [7,1,2,3,4,5,6]
-     * rotate 2 steps to the right: [6,7,1,2,3,4,5]
-     * rotate 3 steps to the right: [5,6,7,1,2,3,4]
-     */
-    public void rotate(int[] nums, int k) {
-        rotate1(nums, k);
-    }
-
-    /**
      * 效率较低，易理解
      */
-    private void rotate1(int[] nums, int k) {
+    private void rotate(int[] nums, int k) {
         k = k % nums.length;
 
         int temp;
@@ -41,8 +36,8 @@ public class RotateArray {
     /**
      * 看一种O(n)的空间复杂度的方法，复制一个和 nums 一样的数组，然后利用映射关系i -> (i+k)%n来交换数字
      */
-    private void rotate2(int[] nums, int k) {
-        k =  k % nums.length;
+    private void rotate1(int[] nums, int k) {
+        k = k % nums.length;
 
         int[] t = nums.clone();
 
