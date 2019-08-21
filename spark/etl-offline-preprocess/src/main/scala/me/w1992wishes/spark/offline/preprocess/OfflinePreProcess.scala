77a1e615f69daed15e5a6bdb8d99eed87c67cc13
w1992wishes <571716844@qq.com>
2019-08-21 19:16:28 +0800

    spark base
    Signed-off-by: w1992wishes <571716844@qq.com>

--- spark/etl-offline-preprocess/src/main/scala/me/w1992wishes/spark/offline/preprocess/OfflinePreProcess.scala
+++ spark/etl-offline-preprocess/src/main/scala/me/w1992wishes/spark/offline/preprocess/OfflinePreProcess.scala
@@ -1,7 +1,7 @@
 package me.w1992wishes.spark.offline.preprocess
 
 import me.w1992wishes.spark.offline.preprocess.config.CommandLineArgs
-import me.w1992wishes.spark.offline.preprocess.core.{PreProcess, PreProcessOnGpPartitions, PreProcessOnRDBMS}
+import me.w1992wishes.spark.offline.preprocess.core.{PreProcess, PreProcessOnGpPartitions}
 
 /**
   * @author w1992wishes 2019/2/26 13:52
@@ -10,12 +10,7 @@ object OfflinePreProcess {
 
   def main(args: Array[String]): Unit = {
     val commandLineArgs: CommandLineArgs = new CommandLineArgs(args)
-    val preProcessJob: PreProcess =
-      commandLineArgs.preProcessImpl match {
-        case 1 => new PreProcessOnRDBMS(commandLineArgs)
-        case 2 => new PreProcessOnGpPartitions(commandLineArgs)
-        case _ => new PreProcessOnRDBMS(commandLineArgs)
-      }
+    val preProcessJob: PreProcess = new PreProcessOnGpPartitions(commandLineArgs)
     println(s"======> ${preProcessJob.getClass}")
     preProcessJob.wholePreProcess()
   }
---
+++ spark/spark-base/src/main/scala/me/w1992wishes/spark/base/transform/Intersection.scala
@@ -0,0 +1,20 @@
+package me.w1992wishes.spark.base.transform
+
+import org.apache.spark.sql.SparkSession
+
+/**
+  * @author w1992wishes 2019/8/21 11:42
+  */
+object Intersection {
+
+  def main(args: Array[String]): Unit = {
+    val spark: SparkSession = SparkSession.builder().master("local").getOrCreate()
+
+    val rdd1 = spark.sparkContext.parallelize(Seq(1, 2, 3, 4, 5, 6, 7, 8, 9), 5)
+    val rdd2 = spark.sparkContext.parallelize(Seq(2, 4, 6, 8), 2)
+
+    // 默认 parition 数量取 max(numPartitions(parent rdd 1), ... , numPartitions(parent rdd n))
+    println(rdd1.intersection(rdd2).getNumPartitions)
+  }
+
+}
