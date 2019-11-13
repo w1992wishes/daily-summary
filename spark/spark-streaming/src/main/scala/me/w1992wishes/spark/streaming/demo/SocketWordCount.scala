bcff62206e8cc1f921a91201f8e1c01c6d3000c9
w1992wishes <571716844@qq.com>
2019-11-13 20:44:39 +0800

    spark streaming socker demo
    Signed-off-by: w1992wishes <571716844@qq.com>

---
+++ spark/spark-streaming/src/main/scala/me/w1992wishes/spark/streaming/demo/SocketWordCount.scala
@@ -0,0 +1,55 @@
+package me.w1992wishes.spark.streaming.demo
+
+import org.apache.spark.sql.SparkSession
+
+/**
+  * @author w1992wishes 2019/11/13 20:05
+  */
+object SocketWordCount {
+
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession
+      .builder
+      .master("local")
+      .appName("StructuredNetworkWordCount")
+      .getOrCreate()
+
+    // Create DataFrame representing the stream of input lines from connection to localhost:9999
+    val lines = spark.readStream
+      .format("socket")
+      .option("host", "localhost")
+      .option("port", 9999)
+      .load()
+
+    import spark.implicits._
+    // Split the lines into words
+    val words = lines.as[String].flatMap(_.split(" "))
+
+    // Generate running word count
+    val wordCounts = words.groupBy("value").count()
+
+    // Start running the query that prints the running counts to the console
+    val query = wordCounts.writeStream
+      .outputMode("complete")
+      .format("console")
+      .start()
+
+    query.awaitTermination()
+
+    /**
+      * 需要执行 Netcat 来向 localhost:9999 发送数据，比如
+      *
+      * linux
+      * nc -lk 9999
+      * apache spark
+      * apache hadoop
+      * ...
+      *
+      * windows
+      * C:\Users\Administrator>nc -l -p 9999
+      * apache spark
+      * apache hadoop
+      */
+  }
+
+}
