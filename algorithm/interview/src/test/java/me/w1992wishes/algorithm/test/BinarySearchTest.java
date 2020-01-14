17a6525bdf7e473f814f0d581231ba0cf0006595
w1992wishes <571716844@qq.com>
2020-01-14 16:32:48 +0800

    二分搜索
    Signed-off-by: w1992wishes <571716844@qq.com>

---
+++ algorithm/interview/src/test/java/me/w1992wishes/algorithm/test/BinarySearchTest.java
@@ -0,0 +1,181 @@
+package me.w1992wishes.algorithm.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author w1992wishes 2020/1/14 14:25
+ */
+public class BinarySearchTest {
+
+    private int[] array = {1, 1, 2, 3, 4, 5, 6, 7, 7, 8, 9, 9, 9};
+
+    @Test
+    public void binarySearchTest() {
+        Assert.assertEquals(-1, binarySearch(array, 0));
+        Assert.assertEquals(5, binarySearch(array, 5));
+        Assert.assertEquals(9, binarySearch(array, 8));
+
+        Assert.assertEquals(9, binarySearchFirst(array, 8));
+        Assert.assertEquals(7, binarySearchFirst(array, 7));
+        Assert.assertEquals(10, binarySearchFirst(array, 9));
+        Assert.assertEquals(0, binarySearchFirst(array, 1));
+
+        Assert.assertEquals(1, binarySearchLast(array, 1));
+        Assert.assertEquals(2, binarySearchLast(array, 2));
+        Assert.assertEquals(8, binarySearchLast(array, 7));
+        Assert.assertEquals(12, binarySearchLast(array, 9));
+    }
+
+    private int binarySearch(int[] array, int value) {
+        int low = 0;
+        int high = array.length - 1;
+
+        while (low <= high) {
+
+            int mid = (low + high) >>> 1;
+
+            if (array[mid] < value) {
+                low = mid + 1;
+            } else if (array[mid] > value) {
+                high = mid - 1;
+            } else {
+                return mid;
+            }
+
+        }
+        return -1;
+    }
+
+    /**
+     * 查找第一个等于指定值的元素索引
+     *
+     * 原二分查找在找到一个元素后，检查与上一个元素是否相等
+     * 如果不相等，当前元素就是第一个
+     * 如果相等，继续往前找，mid - 1
+     *
+     * @param array array
+     * @param value value
+     * @return index
+     */
+    private int binarySearchFirst(int[] array, int value) {
+        int low = 0;
+        int high = array.length - 1;
+
+        while (low <= high) {
+
+            int mid = (low + high) >>> 1;
+
+            if (array[mid] < value) {
+                low = mid + 1;
+            } else if (array[mid] > value) {
+                high = mid - 1;
+            } else {
+                /*while (mid > 0 && array[mid - 1] == value) {
+                    mid = mid - 1;
+                }
+                return mid;*/
+
+                if (mid == 0 || array[mid - 1] != value) {
+                    return mid;
+                } else {
+                    // 如果前面还有相等的就继续二分查找
+                    high = mid - 1;
+                }
+            }
+
+        }
+        return -1;
+    }
+
+    /**
+     * 查找最后一个等于指定值的元素索引
+     *
+     * @param array array
+     * @param value value
+     * @return index
+     */
+    private int binarySearchLast(int[] array, int value) {
+        int low = 0;
+        int high = array.length - 1;
+
+        while (low <= high) {
+
+            int mid = (low + high) >>> 1;
+
+            if (array[mid] < value) {
+                low = mid + 1;
+            } else if (array[mid] > value) {
+                high = mid - 1;
+            } else {
+               /* while (mid < array.length - 1 && array[mid + 1] == value) {
+                    mid = mid + 1;
+                }
+                return mid;*/
+                if (mid == array.length - 1 || array[mid + 1] != value) {
+                    return mid;
+                } else {
+                    // 如果前面还有相等的就继续二分查找
+                    low = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 查找最后一个小于等于给定值的元素索引
+     *
+     * @param array array
+     * @param value value
+     * @return index
+     */
+    private int binarySearchLastLess(int[] array, int value) {
+        int low = 0;
+        int high = array.length - 1;
+
+        while (low <= high) {
+
+            int mid = (low + high) >>> 1;
+            if (array[mid] <= value) {
+                if (mid == array.length - 1 || array[mid + 1] > value) {
+                    return mid;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                high = mid - 1;
+            }
+
+        }
+        return -1;
+    }
+
+    /**
+     * 查找第一个大于等于指定值的元素索引
+     *
+     * @param array array
+     * @param value value
+     * @return index
+     */
+    public int binarySearchFistMore(int[] array, int value) {
+        int low = 0;
+        int high = array.length - 1;
+
+        while (low <= high) {
+
+            int mid = low + ((high - low) >> 1);
+
+            if (array[mid] >= value) {
+                if (mid == 0 || array[mid - 1] < value) {
+                    return mid;
+                } else {
+                    high = mid - 1;
+                }
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
