17e3b3cddb3140a1f5a57b74a9d1887fa96de809
w1992wishes <571716844@qq.com>
2021-09-01 10:58:48 +0800

    update

--- leet-code/to-offer/src/main/java/me/w1992wishes/to/offer/array/ThreeSum.java
+++ leet-code/to-offer/src/main/java/me/w1992wishes/to/offer/array/ThreeSum.java
@@ -9,39 +9,32 @@ import java.util.List;
  */
 public class ThreeSum {
     public List<List<Integer>> threeSum(int[] nums) {
-        if (nums == null || nums.length < 3)
-            return new ArrayList<>();
-
         List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length == 0)
+            return res;
+        int len = nums.length;
+        Arrays.sort(nums);  //第一步：先排序
 
-        Arrays.sort(nums); // O(nlogn)
-
-        // 利用双指针，外层套一个 for 循环
-        int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            // 优化1
-            if (nums[i] > 0) {
-                break;
-            }
-            // 优化2
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-            int left = i + 1;
-            int right = n - 1;
-            while (left < right) {
-                int total = nums[i] + nums[left] + nums[right];
-                if (total == 0) {
-                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    while (left < right && nums[left] == nums[left + 1]) {
-                        left++;
-                    }
-                    left++;
-                } else if (total < 0) {
-                    left++;
-                } else {
-                    right--;
-                }
+        //依次遍历每一个元素，把它作为a，在后续的元素里找b+c=-a;
+        for (int i = 0; i < len - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;     //重复元素直接跳过
+            //找两数之和为-a，双指针法
+            int low = i + 1, high = len - 1;
+            while (low < high) {
+                if (nums[low] + nums[high] == -nums[i]) { //找到了一个解
+                    res.add(Arrays.asList(nums[i], nums[low], nums[high]));
+                    //已经找到了，重复的去掉
+                    while (low < high && nums[low + 1] == nums[low])
+                        low++;
+                    while (low < high && nums[high - 1] == nums[high])
+                        high--;
+                    low++;
+                    high--;
+                } else if (nums[low] + nums[high] < -nums[i])
+                    low++;
+                else
+                    high--;
             }
         }
         return res;
