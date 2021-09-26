766e1920178aea04e5f4b991662bff5f34a70db9
w1992wishes <571716844@qq.com>
2021-09-26 18:20:00 +0800

    calcite: csv calcite sql example“

---
+++ calcite/src/main/java/me/w1992wishes/calcite/csv/CsvSchema.java
@@ -0,0 +1,35 @@
+package me.w1992wishes.calcite.csv;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.io.Resources;
+import org.apache.calcite.schema.impl.AbstractSchema;
+import org.apache.calcite.util.Source;
+import org.apache.calcite.util.Sources;
+
+import java.net.URL;
+import java.util.Map;
+
+public class CsvSchema extends AbstractSchema {
+
+    private Map tableMap;
+    private String dataFile;
+
+    public CsvSchema(String dataFile) {
+        this.dataFile = dataFile;
+    }
+
+    @Override
+
+    protected Map getTableMap() {
+        URL url = Resources.getResource(dataFile);
+        Source source = Sources.of(url);
+        if (tableMap == null) {
+            final ImmutableMap.Builder builder = ImmutableMap.builder();
+            builder.put(this.dataFile.split("\\.")[0], new CsvTable(source));
+            // 一个数据库有多个表名，这里初始化，大小写要注意了,TEST01是表名。
+            tableMap = builder.build();
+        }
+        return tableMap;
+
+    }
+}
